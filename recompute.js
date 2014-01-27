aa91b613af1fdc0847a97a659e4152f12c3740b1
Howard Tang <aychtang@gmail.com>
2014-01-27 17:19:20 +0000

    Document recompute.js

--- recompute.js
+++ recompute.js
@@ -8,6 +8,7 @@ var requireFlush = function() {
 	setTimeout(flush, 0);
 };
 
+// Computes all computations that have been invalidated since last flush.
 var flush = function() {
 	for (var i = 0; i < toFlush.length; i++) {
 		toFlush[i].compute();
@@ -15,13 +16,21 @@ var flush = function() {
 	toFlush = [];
 };
 
+
+// Searches for obj within an array returns true if found
+// or undefined if not found.
 var contains = function(array, obj) {
 	for (var i = 0; i < array.length; i++) {
 		if (array[i] === obj) return true;
 	}
 };
 
-// Keeps reference to a function.
+// Computation
+// Keeps reference to a function, and makes sure it is run
+// when the Computation has been invalidated.
+
+// Whenever a Dependency that a computation depends upon is changed,
+// that Computation is invalidated.
 var Computation = function(f) {
 	this.id = nextId++;
 	this.onInvalidateCallbacks = [];
@@ -36,7 +45,8 @@ Computation.prototype.compute = function() {
 	currentComputation = null;
 };
 
-// Adds to flush list.
+// Adds the current computation to flush list.
+// Schedules a flush.
 Computation.prototype.invalidate = function() {
 	if (!contains(toFlush, this)) {
 		toFlush.push(this);
@@ -48,12 +58,18 @@ Computation.prototype.invalidate = function() {
 	this.onInvalidateCallbacks = [];
 };
 
-// Tracks dependant functions and invalidates upon change.
+// Dependency
+// When Computations depend on a reactive value, they are added
+// to the dependents list of the Dependency object that is 
+// associated with that reactive value.
+
+// A Dependency has a .changed() method available, which when
+// called, will invalidate every Computation that depends on it.
 var Dependency = function() {
 	this.dependents = {};
 };
 
-// Adds current computation to list of dependants of this dependency.
+// Adds current computation to list of dependants of this Dependency.
 Dependency.prototype.depend = function() {
 	var self = this;
 
@@ -66,13 +82,15 @@ Dependency.prototype.depend = function() {
 	}
 };
 
+// Invalidates all Computations that depend on this Dependency.
 Dependency.prototype.changed = function() {
 	for (var dep in this.dependents) {
 		this.dependents[dep].invalidate();
 	}
 };
 
-// autorun creates new computation and runs once.
+// Autorun creates new computation representing the function that
+// has been passed in, and computes it once.
 autorun = function(f) {
 	var c = new Computation(f);
 	c.compute();
